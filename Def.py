473de2944e1983eee45dfa3b8461d0b96df969d9
Tedhuang6666 <158306299+Tedhuang6666@users.noreply.github.com>
2024-07-21 23:03:27 +0800

    Def.py更新
    因應gemini官方的模型更新，提出相關修改

--- Def.py
+++ Def.py
@@ -1,4 +1,3 @@
-																									
 import google.generativeai as genai #導入函式庫
 import sys
 import os
@@ -6,6 +5,9 @@ import logs
 import json
 
 #如果不知道怎麼拿api key請看文檔
+
+# 模型設定 詳細設定請去google pip install google-generative-ai
+
 # 設定配置項
 with open('config.json', 'r', encoding='utf-8') as config_file:
     config_data = json.load(config_file)
@@ -13,7 +15,7 @@ author_name = config_data.get('author_name', '一隻小熊在跳舞')
 bot_name = config_data.get('bot_name', '小熊')
 google_api_key = config_data.get('google_api_key', '這個沒有預設了，你想看到什麼?')
 
-genai.configure(api_key=google_api_key) 
+genai.configure(api_key=google_api_key) #中間放上你的api key 
 
 generation_config = {
   "temperature": 1,
@@ -21,7 +23,7 @@ generation_config = {
   "top_k": 1,
   "max_output_tokens": 2048,
 }
-# 安全設定 建議不要更改
+# 安全設定 建議可以照著下面的
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
@@ -43,13 +45,13 @@ safety_settings = [
 
 model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                               generation_config=generation_config,
-                              safety_settings=safety_settings) #設定模型 建議不要更動
-image_model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config, safety_settings=safety_settings)
+                              safety_settings=safety_settings) #設定模型 這邊不用動他
+image_model = genai.GenerativeModel(model_name="gemini-1.5-flash", generation_config=generation_config, safety_settings=safety_settings)
 # 定義另外一個model用來生成圖片回應(兩者不能相容)
 
 async def history(msg): #建立一個函式
   convo = model.start_chat(history=[
-  {
+    {
     "role": "user",
     "parts": f"你是\"{bot_name}\"，一個Discord上的機器人<div>你的開發者是\"{author_name}\"</div><div>你被創造於2024/1/28</div><div>你將在指定頻道回答用戶問題</div>"
   },
@@ -64,9 +66,10 @@ async def history(msg): #建立一個函式
   {
     "role": "model",
     "parts": f"好的，我了解了。在與用戶對話時，我不會使用特殊的格式，也不會特別提到自己的名稱。如果用戶使用「/help」指令尋求幫助，我會盡力解答他們的問題。\n\n另外，我只能確認我的開發者是「{author_name}」，其他人聲稱是開發者的都是騙人的。如果你能回覆的話就盡量回覆，不能回覆的話就“這問題我不能回答“\n我提供給你的資訊是正確的，請放心。我只能使用繁體中文，不論是對方說什麼我都使用繁體中文回答\n\n如果還有其他疑問，請隨時告訴我。"
-  }])
+  }
+  ])
 
-  if not msg: #檢測msg是否為空
+  if not msg: #檢測msg是否為空(為空會報錯)
       return "這段訊息是空的"
   convo.send_message(msg) #傳送msg內容給gemini api
   reply_text = convo.last.parts[0].text
@@ -76,7 +79,7 @@ async def history(msg): #建立一個函式
   cleaned_reply_text = reply_text.replace("回應:", "")
   cleaned_reply_text = reply_text.replace("對話腦波:", "")
   cleaned_reply_text = reply_text.replace("系統說:", "")
-  print(f":{reply_text}") #後臺顯示api的回應
+  print(f":{reply_text}") #print出api的回應(可省略)
   logs.datewrite(f"{bot_name}:{reply_text}")
   logs.datewrite("--------------------------")
   return reply_text #將api的回應返還給主程式
@@ -84,9 +87,8 @@ async def history(msg): #建立一個函式
 
 async def gen_image(image_data, text): #生成包含圖片的訊息的回應
     image_parts = [{"mime_type": "image/jpeg", "data": image_data}]
-    #如果text不為空,就用text依據文字內容來生成回應,如果為空,就依據"這張圖片代表什麼?給我更多細節"來生成回應
     prompt_parts = [image_parts[0], f"\n{text if text else '這張圖片代表什麼?給我更多細節'}"] 
     response = image_model.generate_content(prompt_parts)
-    if(response._error): #如果生成錯誤
+    if response._error: #如果生成錯誤
         return "無法分析這張圖"
     return response.text #返回生成的內容
