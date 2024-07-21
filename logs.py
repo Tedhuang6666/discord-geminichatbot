2dd172c97801b7ff96b89ebae42cb20775697284
Tedhuang6666 <158306299+Tedhuang6666@users.noreply.github.com>
2024-07-21 23:02:08 +0800

    logs.py 日誌bug修復/更改檔案儲存方式
    修正原 「創建過多檔案」問題
    更改為每日一檔案儲存

--- logs.py
+++ logs.py
@@ -5,16 +5,11 @@ def datewrite(content):
     # 取得當前日期
     current_date = datetime.today().strftime("%Y-%m-%d")
     log_dir = 'log'
-    log_file_path = os.path.join(log_dir, f'{current_date}-1.txt')  # 初始運行次數為1
+    log_file_path = os.path.join(log_dir, f'{current_date}.txt')  
     
-    # 檢查是否需要創建新的日誌文件，以及計算運行次數
+    # 檢查是否需要創建新的日誌文件
     if not os.path.exists(log_file_path):
         os.makedirs(log_dir, exist_ok=True)  # 確保log目錄存在
-    else:
-        run_count = 1
-        while os.path.exists(log_file_path):
-            run_count += 1
-            log_file_path = os.path.join(log_dir, f'{current_date}-{run_count}.txt')
     
     # 取得當前時間
     current_time = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
